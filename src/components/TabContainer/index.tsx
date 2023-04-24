a33b4b761e4606c096025fbec6cb0fced7818fd2
DEWITT SCOTT CHAVEZ PONCE <dschavezp@gmail.com>
2023-04-24 16:53:25 -0500

    removing components

--- src/components/TabContainer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ComponentProps } from "types";
-
-function TabContainer({ children }: ComponentProps) {
-  return <div className="tabs px-4 py-2">{children}</div>;
-}
-
-export default TabContainer;
